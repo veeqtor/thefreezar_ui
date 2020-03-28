888fdf6c79ba0ddfa26a0eab0ad6bdc274d80fd3
Veeqtor <nwokeochavictor@gmail.com>
2020-03-28 16:33:57 +0100

    chore(setup): Setup development environment
    - Setup react with redux saga
    - setup testing with jest and react testing tool
    
    closes #1

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import SEO from '../../components/layouts/seo';
+import SEO from '../../components/layouts/SEO';
 import { goToNextPage, retrieveLastPageState } from '../../store/actions/navigation';
 import { IApplicationRootState } from '../../types';
 
